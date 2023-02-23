d139c4f1df43cd6ffc630e0b32fc4fe487a0d76b
student <student@admins-MacBook-Pro.local>
2023-02-22 18:14:38 -0800

    js stuff

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+console.log("hello");
+function myOnClickFunction(){
+    alert("You clicked this button!");
+    document.querySelector("#button").innerText =  "clicked!";
+    document.querySelector("#button").style.backgroundColor = "blue";
+}
